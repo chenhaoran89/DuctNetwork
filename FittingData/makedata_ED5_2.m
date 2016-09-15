4ed510dde88245b8d56f598c738f1c7030876085
ccui003 <ccui003@e.ntu.edu.sg>
2016-09-15 14:42:16 +0800

    modify tables

--- FittingData/makedata_ED5_2.m
+++ FittingData/makedata_ED5_2.m
@@ -1,21 +1,20 @@
-Cb = zeros(11,11,11);%Qb/Qc,AbAc,AsAc
-Cs = zeros(11,11,11);%Qs/Qc,AbAc,AsAc
-
 FileName='ED5_2.txt';
-for ii=1:11
-    Cb(:,:,ii) = Get3DTable(FileName, (ii-1)*11+6,(ii)*11+5)';
-    Cs(:,:,ii) = Get3DTable(FileName, (ii-1)*11+133,(ii)*11+132)';
-end
+
+Cb = zeros(11,11,11);
+Cb2 = zeros(11,11,11);
+Cs = zeros(11,11,11);
+Cs2 = zeros(11,11,11);
 
 QbQc = [0.01      0.1      0.2      0.3      0.4      0.5      0.6      0.7      0.8      0.9      1.0];
 QsQc = [0.01      0.1      0.2      0.3      0.4      0.5      0.6      0.7      0.8      0.9      1.0];
 AbAc = [0.01      0.1      0.2      0.3      0.4      0.5      0.6      0.7      0.8      0.9      1.0];
 AsAc = [0.01      0.1      0.2      0.3      0.4      0.5      0.6      0.7      0.8      0.9      1.0];
 
-[N1,N2,N3] = size(Cb);
-Cb = Cb.*repmat(reshape(QbQc',N1,1,1),1,N2,N3)./repmat(reshape(AbAc,1,N2,1),N1,1,N3).^2;
+for ii=1:11
+    Cb2(:,:,ii) = Get3DTable(FileName, (ii-1)*11+6,(ii-1)*11+16)';
+    Cb(:,:,ii) = Cb2(:,:,ii).*(QbQc'.^2*AbAc.^-2);
+    Cs2(:,:,ii) = Get3DTable(FileName, (ii-1)*11+133,(ii-1)*11+143)';
+    Cs(:,:,ii) = Cs2(:,:,ii).*(QsQc'.^2*(AsAc(ii)*ones(1,11)).^-2);
+end
 
-[N1,N2,N3] = size(Cs);
-Cs = Cs.*repmat(reshape(QsQc',N1,1,1),1,N2,N3)./repmat(reshape(AsAc,1,1,N3),N1,N2,1).^2;
-
-save('ED5_2.mat','Cb','Cs','QbQc','QsQc','AsAc','AbAc');
+save('ED5_2.mat','Cb','Cb2','QbQc','AbAc','Cs','Cs2','QsQc','AsAc');
--- FittingData/makedata_ER5_1.m
+++ FittingData/makedata_ER5_1.m
@@ -1,21 +1,20 @@
-Cb = zeros(11,3,3);%Qb/Qc,AbAc,AsAc
-Cs = zeros(11,3,3);%Qs/Qc,AbAc,AsAc
-
 FileName='ER5_1.txt';
-for ii=1:3
-    Cb(:,:,ii) = Get3DTable(FileName, (ii-1)*3+6,(ii)*3+5)';
-    Cs(:,:,ii) = Get3DTable(FileName, (ii-1)*3+21,(ii)*3+20)';
-end
+
+Cb = zeros(11,3,3);
+Cb2 = zeros(11,3,3);
+Cs = zeros(11,3,3);
+Cs2 = zeros(11,3,3);
 
 QbQc = [0.01      0.1      0.2      0.3      0.4      0.5      0.6      0.7      0.8      0.9      1.0];
 QsQc = [0.01      0.1      0.2      0.3      0.4      0.5      0.6      0.7      0.8      0.9      1.0];
 AbAc = [0.25      0.5      1.0];
 AsAc = [0.5      0.75      1.0];
 
-[N1,N2,N3] = size(Cb);
-Cb = Cb.*repmat(reshape(QbQc',N1,1,1),1,N2,N3)./repmat(reshape(AbAc,1,N2,1),N1,1,N3).^2;
+for ii=1:3
+    Cb2(:,:,ii) = Get3DTable(FileName, (ii-1)*3+6,(ii-1)*3+8)';
+    Cb(:,:,ii) = Cb2(:,:,ii).*(QbQc'.^2*AbAc.^-2);
+    Cs2(:,:,ii) = Get3DTable(FileName, (ii-1)*3+21,(ii-1)*3+23)';
+    Cs(:,:,ii) = Cs2(:,:,ii).*(QsQc'.^2*(AsAc(ii)*ones(1,3)).^-2);
+end
 
-[N1,N2,N3] = size(Cs);
-Cs = Cs.*repmat(reshape(QsQc',N1,1,1),1,N2,N3)./repmat(reshape(AsAc,1,1,N3),N1,N2,1).^2;
-
-save('ER5_1.mat','Cb','Cs','QbQc','QsQc','AsAc','AbAc');
+save('ER5_1.mat','Cb','Cb2','QbQc','AbAc','Cs','Cs2','QsQc','AsAc');
